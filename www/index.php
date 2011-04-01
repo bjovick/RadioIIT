7b53d41d81d8e1dc911a35451829da339548da67
Tony Lara <celtichawk@gmail.com>
2011-04-01 15:41:58 -0500

    primera version del sistema de vistas de paginas

--- www/index.php
+++ www/index.php
@@ -30,14 +30,8 @@
 	</header>
 	<div id="main">
 		<div id="mainbar">
-		<?php echo Vista::hello(); ?>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean</p>
-
-<p>Vivamus fermentum semper porta. Nunc diam velit, adipiscing ut tristique vitae, sagittis vel odio. Maecenas convallis ullamcorper ultricies. Curabitur ornare, ligula semper consectetur sagittis, nisi diam iaculis velit, id fringilla sem nunc vel mi. Nam dictum, odio nec pretium volutpat, arcu ante placerat erat, non tristique elit urna et turpis. Quisque mi metus, ornare sit amet fermentum et, tincidunt et orci. Fusce eget orci a orci congue vestibulum. Ut dolor diam, elementum et vestibulum eu, porttitor vel elit.</p>
-<p>Curabitur venenatis pulvinar tellus gravida ornare. Sed et erat faucibus nunc euismod ultricies ut id justo. Nullam cursus suscipit nisi, et ultrices justo sodales nec. Fusce venenatis facilisis lectus ac semper. Aliquam at massa ipsum. Quisque bibendum purus convallis nulla ultrices ultricies. Nullam aliquam, mi eu aliquam tincidunt, purus velit laoreet tortor, viverra pretium nisi quam vitae mi. Fusce vel volutpat elit. Nam sagittis nisi dui.</p>
 		</div>
 		<div id="sidebar">
-			<p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam eu ante dolor, a dignissim est. Vivamus congue nunc vel lacus interdum luctus. Mauris consequat gravida interdum. Sed sit amet augue ac ante hendrerit facilisis. Aliquam tellus nisi, interdum a sollicitudin vitae, malesuada ac diam. Ut mauris nibh, aliquet eu venenatis in, tempor ut enim. Phasellus aliquam turpis sit amet massa gravida accumsan. Nulla posuere gravida nulla, et sodales libero faucibus id. Sed bibendum faucibus lectus, eu blandit quam pretium at.</p>
 		</div>
 		<br class="clear" />
 	</div>
--- www/sistema/Vista.php
+++ www/sistema/Vista.php
@@ -1,7 +1,92 @@
 <?php defined('SISPATH') or die('No direct script access.');
 
 class Vista { 
-	public static function hello() {
-		return 'hola como estan';
+	private $_vars;
+	private $_archivo;
+
+	// Methodos Magicos //
+	public function __construct($vista, array $params = null) {
+		$this->set_filename($vista);
+		if ($params != null) {
+			$this->_vars = $params + $this->_vars;
+		}
+	}
+
+	public function __set($key, $value) {
+		$this->set($key, $value);
+	}
+
+	public function __isset($key) {
+		return isset($this->_vars[$key]);
+	}
+
+	public function __unset($key) {
+		unset($this->_vars[$key]);
+	}
+
+	public function __toString() {
+		try {
+			return $this->presentar();
+		} catch (Exception $e) {
+			echo 'Error al intepretart vista: '.$e;
+			return '';
+		}
+	}
+
+	public function set($key, $value = NULL) {
+		if (is_array($key)) {
+			foreach ($key as $name => $value) {
+				$this->_vars[$name] = $value;
+			}
+		} else {
+			$this->_vars[$key] = $value;
+		}
+
+		return $this;
+	}
+
+	public function set_filename($file) {
+		$_file = explode('.', $file);
+		$ext = (count($_file) == 2) ? array_pop($_file) : 'php';
+		$file = $_file[0].'.'.$ext;
+		if(!file_exists(VISTASPATH.$file)) {
+			throw new Exception('No se pudo encontrar la vista '.$file.'.');
+		}
+
+		$this->_archivo = VISTASPATH.$file;
+		return $this;
+	}
+
+	public function presentar($file = NULL) {
+		if ($file !== NULL) {
+			$this->set_filename($file);
+		}
+
+		if (empty($this->_archivo)) {
+			throw new Exception('You must set the file to use within your view before rendering');
+		}
+
+		return $this->capturar();
+	}
+
+	protected static function capturar() {
+		// importar las variables de la vista al contexto local
+		extract($this->_vars, EXTR_SKIP);
+
+		// empezar a capturar el buffer de la vista
+		ob_start();
+
+		try {
+			// cargando el archivo de la vista
+			include $this->_archivo;
+		}	catch (Exception $e) {
+			// limpiando el buffer antes de mandar la excepcion
+			ob_end_clean();
+
+			throw $e;
+		}
+
+		// capturar el resultado y limpiar el buffer
+		return ob_get_clean();
 	}
 }
