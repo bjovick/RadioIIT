41b51af52698a32db8736608d11e2c1d230d2266
Tony Lara <celtichawk@gmail.com>
2011-04-04 14:40:45 -0500

    arreglado el rewrite pero solo remoto

--- www/sistema/URL.php
+++ www/sistema/URL.php
@@ -9,6 +9,14 @@
 class URL {
 	public static function obtener() {
 		$url_arr = array();
-
+		/**
+		 * http://radioiit.tonylara.net/werewkjwe/kwunjnhf?ewer&weiwe=0932
+		 * [QUERY_STRING] => ewer&weiwe=0932
+		 * [REDIRECT_QUERY_STRING] => ewer&weiwe=0932
+		 * [REDIRECT_URL] => /werewkjwe/kwunjnhf
+		 * [REQUEST_URI] => /werewkjwe/kwunjnhf?ewer&weiwe=0932
+		 * [SCRIPT_FILENAME] => /home/tonylar1/public_html/radioiit/index.php
+		 * [SCRIPT_NAME] => /index.php
+		 */
 	}
 }
