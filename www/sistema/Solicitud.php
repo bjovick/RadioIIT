45dcb307edf19ea813f2e34ae1e3ef5c332f18d1
Tony Lara <celtichawk@gmail.com>
2011-04-06 09:53:13 -0500

    cambiando a usar kohana

--- www/sistema/Solicitud.php
+++
@@ -1,6 +0,0 @@
-<?php defined('SISPATH') or die('No se permite accesso directo al archivo.');
-
-class Solicitud {
-	public static function instancia() {
-	}
-}
--- www/vistas/blocks/login.php
+++
@@ -1,1 +0,0 @@
-el formulario de login querido <?php echo $n; ?>
