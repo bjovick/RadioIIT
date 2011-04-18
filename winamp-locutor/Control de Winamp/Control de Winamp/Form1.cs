7fc4f4c8f2e3ddbc32274df3e35b61141158a430
Miguel Navarrete <b_jovick@hotmail.com>
2011-04-18 13:48:04 -0600

    Parte del proyecto c#

---
+++ winamp-locutor/Control de Winamp/Control de Winamp/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Threading;
+using System.IO;
+using MySql;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+
+
+
+namespace Control_de_Winamp
+{
+    public partial class Form1 : Form
+    {
+        public const int WM_USER = 0x400;
+        public const int WM_COMMAND = 0x0111;
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("user32.dll", CharSet=CharSet.Auto)]
+        private static extern IntPtr SendMessageA(IntPtr hWnd, int msg, int wParam, int lParam);
+        [DllImport("User32.Dll")]
+        public static extern void GetWindowText(int h, StringBuilder s, int nMaxCount);
+        IntPtr hwndWinamp;
+        public Form1()
+        {
+            InitializeComponent();
+            hwndWinamp = FindWindow("Winamp v1.x",null);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MySqlConnection connection = new MySqlConnection("Server=184.154.79.138;Database=tonylar1_radioiit;Uid=tonylar1_iitbd;Pwd=password890;");
+            connection.Open();
+            //if (connection.State==ConnectionState.Open)
+            //    MessageBox.Show("yeaahhh");
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+            if (hwndWinamp==IntPtr.Zero)
+            {
+                //dir appdata %APPDATA%\Winamp\
+                ProcessStartInfo dir = new ProcessStartInfo(@"C:\Program Files\Winamp\Winamp.exe");
+                Process wnmpPlaylist = Process.Start(dir);
+                Thread.Sleep(3000);
+                hwndWinamp = FindWindow("Winamp v1.x",null);                
+            }
+
+        }
+
+        private void actualizarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //string[] filePaths = Directory.GetFiles(@"", "*.mp3", SearchOption.AllDirectories);
+            Form2 actualizarDlg = new Form2();
+            actualizarDlg.Show();           
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }    
+    }
+}
---
+++ winamp-locutor/Control de Winamp/Control de Winamp/Form2.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Control_de_Winamp
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(362, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Selecciona la carpeta donde se encuentran todas las canciones, por favor.";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(35, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox1.Size = new System.Drawing.Size(275, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(316, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(73, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Examinar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(32, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "NOTA: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(61, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(250, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Este apartado sólo actualiza el catálogo mp3 en el sitio Web.";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(142, 79);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Aceptar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.DesktopDirectory;
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 114);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form2";
+            this.Text = "Directorio de canciones";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
