bfe758cc6843c9ff01e75a17e6cabba31066416d
Tony Lara <celtichawk@gmail.com>
2011-05-09 18:08:57 -0500

    agregando playlist en la pagina de inicio

--- www/application/classes/controller/inicio.php
+++ www/application/classes/controller/inicio.php
@@ -3,11 +3,26 @@
 class Controller_Inicio extends Controller {
 
 	public function action_index() {
+		$h = Horarios::actual();
+		$p = Playlist::instancia();
+		$canciones = $p->canciones();
+		$canciones[0]['actual'] = true;
+		$playlist = array(
+			'generos' => $h['generos'],
+			'nombre' => $h['nombre'],
+			'canciones' => $canciones,
+		);
+
+
 		$anuncio = Markdown(Model_Contenidos::leer('inicio.anuncio')->get('texto_md'));
 		$intro = Markdown(Model_Contenidos::leer('inicio.intro')->get('texto_md'));
-		$aux = View::factory('bloques/login');
-		$H = Horarios::actual();
-
+		$aux = View::factory('bloques/login').PHP_EOL.
+			 View::factory('bloques/playlist')
+			 ->set('playlist', $playlist)
+				->set('con_cancion_actual', true)
+			 ->set('titulo', 'Tocando: '.(empty($playlist['nombre'])
+																	? $playlist['generos'] : $playlist['nombre']));
+	
 		$inicio_v = View::factory('paginas/inicio')
 								->set('anuncio', $anuncio)
 								->set('intro', $intro)
