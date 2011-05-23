9e9dc6fc2a669f00e9a49d0e49824ad66ccfe19c
Tony Lara <celtichawk@gmail.com>
2011-05-22 19:52:38 -0500

    agregando la funcion para validar emails en la clase Sitio

--- www/application/classes/sitio.php
+++ www/application/classes/sitio.php
@@ -14,6 +14,82 @@ class Sitio {
 		return '6LdlccMSAAAAAKvPCnmTrirZBcVOqn6F_yrmC6gJ';
 	}
 
+	public static function email_valido($email) {
+		$esperar_tcp = 180;
+		$espera_de_smtp = 60; //ayuda a la sincronizacion
+
+		//checar si esta formateado como email
+		if(filter_var($email, FILTER_VALIDATE_EMAIL) === FALSE) {
+			return false; //ni si quiera formateado
+		}
+
+		$tmp = explode('@', $email);
+		$usuario = $tmp[0];
+		$dominio = $tmp[1];
+
+		//recolectar los records MX del servidor
+		if(getmxrr($dominio, $hosts_mx, $peso_mx)) {
+			for($i=0; $i<count($peso_mx); $i++) {
+				$mxs[$hosts_mx[$i]] = $peso_mx[$i];
+			}
+			asort($mxs);
+			$remitentes = array_keys($mxs);
+		} elseif (checkdnsrr($dominio, 'A')) {
+			$remitentes[0] = gethostbyname($dominio);
+		} else {
+			$remitentes = array();
+		}
+
+		$total = count($remitentes);
+
+		//consultar cada servidor de email
+		if($total > 0) {
+			//si aceptan email?
+			for($n=0; $n<$total; $n++) {
+				//si se puede abrir el socket
+				$num_error = 0;
+				$error_s 0;
+				$sock = @fsockopen($remitentes[$n], 25, $num_error, $error_s, $esperar_tcp);
+				if($sock) {
+					$respuesta = fread($sock, 8192);
+					stream_set_timeout($sock, $esperar_tcp);
+					$meta = stream_get_meta_data($sock);
+					$cmds = array {
+						'HELLO radioiit.co.cc',
+						'MAIL FROM: <'.Sitio::config('email_para_recibir_recomendaciones').'>',
+						'RCPT TO: <'.$email.'>',
+						'QUIT',
+					};
+
+					//si no hubo conneccion, break
+					if(!$meta['timed_out'] && !preg_match('/^2\d\d[ -]/', $respuesta)) {
+						$error = true;
+						break;
+					}
+					//mandar comandos de chequeo
+					foreach($cmds as $cmd) {
+						fputs($sock, "$cmd\r\n");
+						$respuesta = fread($sock, 4096);
+						if(!meta['timed_out'] && preg_match('/^d\d\d[ -]/', $respuesta)) {
+							$error = true;
+							break 2;
+						}
+					}
+
+					//cerrar
+					fclose($sock);
+					break;
+				} elseif($n == $total-1) {
+					$error = true;
+				}
+			}
+		} else {
+			$error = true;
+		}
+
+		return !isset($error);
+	}
+
 	/**
 	 * regresa las configuraciones del sitio guardadas en la
 	 * base de datos
